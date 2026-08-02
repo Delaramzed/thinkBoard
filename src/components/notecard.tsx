cc5b997346f9c64793c8e38c70f5330ca77e11ad
delaram <delaram.zed90@gmail.com>
2026-08-02 20:30:43 +0330

    add note list and note card structure

--- src/components/notecard.tsx
+++ src/components/notecard.tsx
@@ -1,10 +1,13 @@
-function NoteCard (){
-    return(
-        <div>
-            <h2></h2>
-            <p></p>
-        </div>
-    )
-
+type Props = {
+  title: string;
+  content: string;
+};
+function NoteCard({ title, content }: Props) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
 }
 export default NoteCard;
--- src/components/notelist.tsx
+++ src/components/notelist.tsx
@@ -1,4 +1,19 @@
-function NoteList() {
-  return <div></div>;
+import NoteCard from "./notecard";
+
+type Note = {
+  title: string;
+  content: string;
+};
+type Props = {
+  notes: Note[];
+};
+function NoteList({ notes }: Props) {
+  return (
+    <div>
+      {notes.map((note) => (
+        <NoteCard title={note.title} content={note.content} />
+      ))}
+    </div>
+  );
 }
 export default NoteList;
