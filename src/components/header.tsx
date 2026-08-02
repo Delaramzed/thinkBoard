89828d09c2d841650776a50b633f64a8970b54f7
delaram <delaram.zed90@gmail.com>
2026-08-02 12:54:39 +0330

    add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="w-full bg-blue-200 h-20 p-5">
+      <nav className="flex h-full px-5">
+        <h1 className="font-bold text-xl text-black pt-3">Think Board</h1>
+      </nav>
+    </header>
+  );
+}
+export default Header;
