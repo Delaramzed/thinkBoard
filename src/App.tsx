a6dcc9cbd1b6455a08ebfbb8de2103aeca8fdfb4
delaram <delaram.zed90@gmail.com>
2026-08-03 09:43:09 +0330

    save notes to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/header";
 import NoteForm from "./components/noteform";
 import NoteList from "./components/notelist";
 import type { Note } from "./components/note";
 
-
 function App() {
-  const [notes , setNotes]=useState<Note[]>([]);
-  const submitForm = (title: string, content: string)=>{
-     const newNote :Note= {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const saveNote = localStorage.getItem("notes");
+  return saveNote ? JSON.parse(saveNote) : [];
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+  const submitForm = (title: string, content: string) => {
+    const newNote: Note = {
       id: Date.now(),
       title,
       content,
     };
-    setNotes([...notes,newNote])
-  }
+    setNotes([...notes, newNote]);
+  };
   return (
     <div className="min-h-screen bg-blue-100">
       <Header />
       <main>
-        <NoteForm submitForm={submitForm}  />
-        <NoteList  notes={notes}/>
+        <NoteForm submitForm={submitForm} />
+        <NoteList notes={notes} />
       </main>
-
     </div>
   );
 }
