79594b5d23fe460cb6b7481135a11f50bfa2ba6b
delaram <delaram.zed90@gmail.com>
2026-08-03 08:56:09 +0330

    add styling to note card

--- src/components/notecard.tsx
+++ src/components/notecard.tsx
@@ -4,9 +4,9 @@ type Props = {
 };
 function NoteCard({ title, content }: Props) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{content}</p>
+    <div className="flex flex-col bg-blue-300 rounded-xl w-100 h-40 px-5 py-4">
+      <h2 className="font-bold text-lg w-full ">{title}</h2>
+      <p className="mt-2 text-gray-600 text-base w-full">{content}</p>
     </div>
   );
 }
